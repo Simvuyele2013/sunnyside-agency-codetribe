809a7af64f3a23d4a38f135164db8ddd467ddde7
Simvuyele2013 <124438571+Simvuyele2013@users.noreply.github.com>
2023-02-28 22:21:41 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+function onLoad() {
+    let heroText = 'WE ARE CREATIVES';
+    let transformText = 'Transform your brand';
+    let transfromtextdescription = 'We are a full service creative agency specializing in helping brands grow fast. Engage your clients through compeling visuals that do most of the marketing for you';
+    let standoutheadingline = 'Stand Out to the right audience';
+    let standoutText = 'Using a collaborative formula of designers, researchers, photographers, videographers and copywriters, we\'ll build and extend your brand in digital spaces.';
+    let graphicdesignheading = 'Graphic Design';
+    let graphicdesigntext = 'Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients attention.';
+    let photographyheading = 'Photography';
+    let photographytext = 'Increase your credibility by getting the most stunning, high-quality photos that improve your business image';
+    let emilymessage = 'We put our trust in Sunnyside and they delivered, making sure our needs were met and deadline hit.';
+    let emilyname = 'Emily R.';
+    let emilyrole = 'Marketing Director';
+    let thomasmessage = "Sunnyside's enthusiasm coupled with their keen interest in our brand's success made it a satisfying and enjoyable experience."
+    let thomasname = 'Thomas S.'
+    let thomasrole =  'Chief Operating Officer'
+    let jenniemessage = ' Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!'
+    let jenniename = 'Jennie F.'
+    let jennierole = 'Business owner'
+
+    document.getElementById("header-text").innerHTML = heroText; 
+    document.getElementById("heading-line").innerHTML = transformText;
+    document.getElementById("transformtextdescription").innerHTML = transfromtextdescription;
+    document.getElementById("standout-heading-line").innerHTML = standoutheadingline;
+    document.getElementById("standouttext").innerHTML = standoutText;
+    document.getElementById("graphic-design-heading").innerHTML = graphicdesignheading;
+    document.getElementById("graphic-design-description").innerHTML = graphicdesigntext;
+    document.getElementById("photographyheading").innerHTML = photographyheading;
+    document.getElementById("photographydescription").innerHTML = photographytext;
+    document.getElementById("emilymessage").innerHTML = emilymessage;
+    document.getElementById("emilyname").innerHTML = emilyname;
+    document.getElementById("emilyrole").innerHTML = emilyrole;
+    document.getElementById("thomasmessage").innerHTML = thomasmessage;
+    document.getElementById("thomasname").innerHTML = thomasname;
+    document.getElementById("thomasrole").innerHTML = thomasrole;
+    document.getElementById("jenniemessage").innerHTML = jenniemessage;
+    document.getElementById("jenniename").innerHTML = jenniename;
+    document.getElementById("jennierole").innerHTML = jennierole;
+
+
+
+
+}
+
+onLoad();
